a7d0098746b1b0419c23b88f562e619b91a9209a
MarcoAntonioCatalano <mcatalano@byupathway.edu>
2026-01-24 12:18:24 -0700

    place exercise

---
+++ week03/scripts/place.js
@@ -0,0 +1,16 @@
+const time = document.querySelector("#time");
+const lastModified = document.querySelector("#lastModified");
+const mainnav = document.querySelector('.navigation');
+const hambutton = document.querySelector('#menu');
+
+const today = new Date();
+
+// 1. Display the current year
+if (time) {
+    time.textContent = today.getFullYear();
+}
+
+// 2. Display the last modified date
+if (lastModified) {
+    lastModified.innerHTML = `Last modified: <span class="highlight">${document.lastModified}</span>`;
+}
